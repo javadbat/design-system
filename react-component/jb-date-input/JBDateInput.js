dfedd9bdca16dc87b5ab7b4a3edf9b7e7c576b31
javad bathaei <javadbat@gmail.com>
2020-08-31 00:25:13 +0430

    add min and max to date input

--- react-component/jb-date-input/JBDateInput.js
+++ react-component/jb-date-input/JBDateInput.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import '../../web-component/jb-date-input'
+import '../../web-component/jb-date-input';
 function JBDateInput(props) {
     return (
-        <jb-date-input label={props.label}></jb-date-input>
+        <jb-date-input label={props.label} min={props.min} max={props.max}></jb-date-input>
     )
 }
 JBDateInput.propTypes = {
-    label: PropTypes.string
+    label: PropTypes.string,
+    min: PropTypes.string,
+    max:PropTypes.string
 }
 export default JBDateInput
