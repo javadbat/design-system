7fa2062e6fcdb2028662c0500fc0da184ca49e15
Hosseini <masmas.hosseini@yahoo.com>
2020-09-21 11:20:21 +0330

    add test jb checkbox

--- test-bed/storybook/stories/JBCheckbox.stories.js
+++ test-bed/storybook/stories/JBCheckbox.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import JBCheckbox from '../../../react-component/jb-checkbox';
 import { action } from '@storybook/addon-actions';
+import JBCheckboxTest from '../stories/pages/JBCheckboxTestPage';
 export default {
     title: 'Example/JBCheckbox',
     component: JBCheckbox
@@ -12,5 +13,10 @@ export const Normal = Template.bind({});
 Normal.args = {
     label:'چک',
     value:false,
-    onChange:(e)=>{action('onChange')(e.target.value)}
+    onChange:(e)=>{action('onChange')(e.target.value);}
+};
+const TestTemplate = (args) => <JBCheckboxTest label={args.label} value={args.value} onChange={args.onChange}></JBCheckboxTest>;
+export const TestCheckbox = TestTemplate.bind({});
+TestCheckbox.args = {
+    label:'چک باکس',
 };
---
+++ test-bed/storybook/stories/pages/JBCheckboxTestPage.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+import JBCheckbox from '../../../../react-component/jb-checkbox';
+function JBCheckboxTest (props){
+    const [value, inputValueSetter] = useState("");
+    return (
+        <JBCheckbox label={props.label} value={value} onChange={e => {inputValueSetter(e.target.value)}}></JBCheckbox>
+    );
+}
+export default JBCheckboxTest;
