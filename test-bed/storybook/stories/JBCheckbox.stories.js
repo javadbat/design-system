1703a7f1ae75c2a66ae0b3aa0ca3b501b880a2bd
Hosseini <masmas.hosseini@yahoo.com>
2020-09-21 10:07:58 +0330

    nothing

--- test-bed/storybook/stories/JBCheckbox.stories.js
+++ test-bed/storybook/stories/JBCheckbox.stories.js
@@ -2,15 +2,15 @@ import React from 'react';
 import JBCheckbox from '../../../react-component/jb-checkbox';
 import { action } from '@storybook/addon-actions';
 export default {
-  title: 'Example/JBCheckbox',
-  component: JBCheckbox
+    title: 'Example/JBCheckbox',
+    component: JBCheckbox
 };
 
 const Template = (args) => <JBCheckbox label={args.label} value={args.value} onChange={args.onChange}></JBCheckbox>;
 
 export const Normal = Template.bind({});
 Normal.args = {
-  label:'چک',
-  value:false,
-  onChange:(e)=>{action('onChange')(e)}
+    label:'چک',
+    value:false,
+    onChange:(e)=>{action('onChange')(e);}
 };
