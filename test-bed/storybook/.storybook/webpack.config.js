1db59ecd6781c50ed88c6aa96f0848646b416fa2
javad bathaei <javadbat@gmail.com>
2021-04-18 02:02:35 +0430

    update searchbar test

--- test-bed/storybook/.storybook/webpack.config.js
+++ test-bed/storybook/.storybook/webpack.config.js
@@ -24,6 +24,7 @@ module.exports = async ({ config }) => {
       'jb-date-input': path.resolve(__dirname, '..','..', '..', 'web-component','jb-date-input', 'dist', 'JBDateInput.js'),
       'jb-time-input': path.resolve(__dirname, '..','..', '..', 'web-component','jb-time-input', 'dist', 'JBTimeInput.js'),
       'jb-time-picker': path.resolve(__dirname, '..','..', '..', 'web-component','jb-time-picker', 'dist', 'JBTimePicker.js'),
+      'jb-searchbar': path.resolve(__dirname, '..','..', '..', 'web-component','jb-searchbar', 'dist', 'JBSearchbar.js'),
       'jb-time-input-react': path.resolve(__dirname, '..','..', '..', 'react-component','jb-time-input', 'dist', 'JBTimeInput.js'),
     }
   }
--- test-bed/storybook/stories/JBSearchbar.stories.js
+++ test-bed/storybook/stories/JBSearchbar.stories.js
@@ -7,6 +7,64 @@ export default {
 const Template = (args) => <JBSearchbar {...args}></JBSearchbar>;
 export const Normal = Template.bind({});
 Normal.args = {
+    placeholder:'select filter',
+    columnList: [
+        {
+            key: 'title',
+            label: 'title',
+            type: 'TEXT'
+        },
+        {
+            key: 'name',
+            label: 'name',
+            type: 'TEXT',
+            maxUsageCount: 3
+        },
+        {
+            key: 'age',
+            label: 'age',
+            type: 'TEXT'
+        },
+        {
+            key: 'fromDate',
+            label: 'from date',
+            type: 'DATE'
+        },
+        {
+            key: 'GENDER',
+            label: 'gender',
+            type: 'SELECT',
+            config: {
+                optionList: [
+                    {
+                        title: 'male',
+                        value: 'MALE'
+                    },
+                    {
+                        title: 'female',
+                        value: 'FEMALE'
+                    }
+                ],
+                getOptionTitle: (option) => {
+                    return option.title;
+                },
+                getOptionValue: (option) => {
+                    return option.value;
+                }
+            }
+
+        },
+        {
+            key: 'fromDate',
+            label: 'to date',
+            type: 'DATE'
+        }
+    ]
+};
+const RTLTemplate = (args) => <div style={{'direction':'rtl'}}><JBSearchbar {...args}></JBSearchbar></div>;
+export const RTLSample = RTLTemplate.bind({});
+RTLSample.args = {
+    placeholder:'نوع فیلتر را انتخاب کنید',
     columnList: [
         {
             key: 'title',
@@ -45,10 +103,10 @@ Normal.args = {
                     }
                 ],
                 getOptionTitle: (option) => {
-                    return option.title
+                    return option.title;
                 },
                 getOptionValue: (option) => {
-                    return option.value
+                    return option.value;
                 }
             }
 
