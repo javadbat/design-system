db9aa7874d0cb9c721665039a9e8a0440f8170c5
javadbat <javadbat@gmail.com>
2022-03-21 21:47:37 +0330

    fix time stamp test

--- test-bed/storybook/stories/pages/JBDateInput/JBDateInputTimeStampTestPage.js
+++ test-bed/storybook/stories/pages/JBDateInput/JBDateInputTimeStampTestPage.js
@@ -1,15 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useMemo} from 'react';
 import JBDateInput from '../../../../../react-component/jb-date-input/lib/JBDateInput';
 function JBDateInputTimeStampTest(props) {
-    const [setValue, setValueSetter] = useState("");
+    const [setValue, setValueSetter] = useState(null);
+    const valueInDate = useMemo(()=>{
+        if(setValue){
+            return new Date(parseInt(setValue)).toString();
+        }else{
+            return null;
+        }
+    },setValue);
     return (
         <div>
-            <JBDateInput label={props.label} valueType={props.valueType} min={props.min} max={props.max} onSelect={e => {setValueSetter(e.target.value);}}></JBDateInput>
+            <JBDateInput value={setValue} label={props.label} valueType="TIME_STAMP" min={props.min} max={props.max} onSelect={e => {setValueSetter(e.target.value);}}></JBDateInput>
             <div>
                 <br /><br />valueType is {props.valueType}
                 <br /><br />Min date is: {props.min? props.min:"Unlimited"}
                 <br /><br />Max date is: {props.max? props.max:"Unlimited"}
                 <br /><br />Your chosen date is: {setValue}
+                <br /><br />Your chosen date in greg is: {valueInDate}
             </div>
         </div>
     );
