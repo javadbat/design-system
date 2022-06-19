39c6a28f19f0cb2ec5a0bfc2365bba4681a6dc35
javadbat <javadbat@gmail.com>
2022-06-19 12:35:52 +0430

    update jb-select to support placeholder

--- react-component/jb-select/lib/JBSelect.js
+++ react-component/jb-select/lib/JBSelect.js
@@ -1,52 +1,54 @@
 /* eslint-disable react/display-name */
-import React, {useEffect, useRef, useState, useImperativeHandle} from 'react';
+import React, { useEffect, useRef, useState, useImperativeHandle } from 'react';
 import 'jb-select';
 import PropTypes from 'prop-types';
 import { useEvent } from '../../custom-hooks/UseEvent';
 
-const JBSelect = React.forwardRef((props, ref)=>{
+const JBSelect = React.forwardRef((props, ref) => {
     let element = useRef();
     const [refChangeCount, refChangeCountSetter] = useState(0);
     useImperativeHandle(
         ref,
-        () => (element?element.current:{}),
+        () => (element ? element.current : {}),
         [element],
     );
     useEffect(() => {
         refChangeCountSetter(refChangeCount + 1);
     }, [element.current]);
     useEffect(() => {
-        element.current.value = props.value ;
+        element.current.value = props.value;
     }, [props.value]);
     useEffect(() => {
-        if(typeof props.getOptionTitle =="function"){
+        if (typeof props.getOptionTitle == "function") {
             element.current.callbacks.getOptionTitle = props.getOptionTitle;
         }
     }, [props.getOptionTitle]);
     useEffect(() => {
-        if(typeof props.getOptionTitle =="function"){
+        if (typeof props.getOptionTitle == "function") {
             element.current.callbacks.getOptionTitle = props.getOptionTitle;
         }
     }, [props.getOptionTitle]);
     useEffect(() => {
-        if(typeof props.getOptionDOM =="function"){
+        if (typeof props.getOptionDOM == "function") {
             element.current.callbacks.getOptionDOM = props.getOptionDOM;
         }
     }, [props.getOptionDOM]);
     useEffect(() => {
-        if(typeof props.getSelectedValueDOM =="function"){
+        if (typeof props.getSelectedValueDOM == "function") {
             element.current.callbacks.getSelectedValueDOM = props.getSelectedValueDOM;
         }
-    } , [props.getSelectedValueDOM]);
+    }, [props.getSelectedValueDOM]);
     useEffect(() => {
         element.current.optionList = props.optionList;
     }, [props.optionList]);
     useEffect(() => {
-        if(props.message !== null && props.message !== undefined){
-            debugger;
+        if (props.message !== null && props.message !== undefined) {
             element.current.setAttribute("message", props.message);
         }
-    },[props.message]);
+    }, [props.message]);
+    useEffect(() => {
+        element.current.setAttribute("placeholder", props.placeholder);
+    }, [props.placeholder]);
     function onKeyup(e) {
         if (props.onKeyup) {
             props.onKeyup(e);
@@ -57,10 +59,10 @@ const JBSelect = React.forwardRef((props, ref)=>{
             props.onChange(e);
         }
     }
-    useEvent(element.current,'keyup',onKeyup);
-    useEvent(element.current,'change',onChange);
+    useEvent(element.current, 'keyup', onKeyup);
+    useEvent(element.current, 'change', onChange);
     return (
-        <jb-select label={props.label} ref={element} required={props.required||false}></jb-select>
+        <jb-select label={props.label} ref={element} required={props.required || false}></jb-select>
     );
 });
 
@@ -68,10 +70,11 @@ const JBSelect = React.forwardRef((props, ref)=>{
 JBSelect.propTypes = {
     label: PropTypes.string,
     optionList: PropTypes.array,
-    getOptionTitle: PropTypes.func, 
+    getOptionTitle: PropTypes.func,
     getOptionValue: PropTypes.func,
     getOptionDOM: PropTypes.func,
     getSelectedValueDOM: PropTypes.func,
+    placeholder: PropTypes.string,
     value: PropTypes.any,
     onChange: PropTypes.func,
     onKeyup: PropTypes.func,
--- test-bed/storybook/stories/JBSelect.stories.js
+++ test-bed/storybook/stories/JBSelect.stories.js
@@ -13,6 +13,7 @@ export const Normal = Template.bind({});
 Normal.args = {
     label:'select from menu',
     message:"please select a value",
+    placeholder:"select number here",
     optionList:['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39'],
 };
 export const OptionObject = Template.bind({});
