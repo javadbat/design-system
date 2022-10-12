1190f56172cd54e94888db812dae61add9fb4b25
mahsa hmd <m.ahmadpoor@hami.blue>
2022-10-12 16:12:58 +0330

    test for jb-pin-input

--- test-bed/storybook/stories/JBPinInput.stories.js
+++ test-bed/storybook/stories/JBPinInput.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {JBPinInput} from '../../../react-component/jb-pin-input/lib/JBPinInput';
+import {JBPinInputStyleTest} from './pages/JBPinInput/JBPinInputStyleTest';
 export default {
     title: 'Example/JBPinInput',
     component: JBPinInput
@@ -11,3 +12,5 @@ CardNumber.args = {
     message:"please input 16 digit card number",
     value:"",
 };
+const PinInputStyleTemplate = () => <JBPinInputStyleTest></JBPinInputStyleTest>;
+export const PinInputStyle = PinInputStyleTemplate.bind({});
