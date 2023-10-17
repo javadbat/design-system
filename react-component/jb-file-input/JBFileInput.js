652c535e060fa1754e22121479475573e2e0c17c
javadbat <javadbat@gmail.com>
2023-10-17 11:01:40 +0330

    remove file input react component

--- react-component/jb-file-input/JBFileInput.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import '../../web-component/jb-file-input'
-function JBFileInput() {
-    return (
-        <jb-file-input></jb-file-input>
-    )
-}
-
-export default JBFileInput
