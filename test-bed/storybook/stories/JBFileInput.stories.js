46dc4b295b604914bd397fc7d5940b3f30fc8930
javadbat <javadbat@gmail.com>
2024-02-17 13:49:18 +0330

    update import problems

--- test-bed/storybook/stories/JBFileInput.stories.js
+++ test-bed/storybook/stories/JBFileInput.stories.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import JBFileInput from '../../../react-component/jb-file-input';
+import {JBFileInput} from '../../../react-component/jb-file-input';
 export default {
     title: 'Example/JBFileInput',
     component: JBFileInput
-  };
-  const Template = (args) => <JBFileInput {...args}></JBFileInput>;
-  export const Normal = Template.bind({});
-  Normal.args = {
+};
+const Template = (args) => <JBFileInput {...args}></JBFileInput>;
+export const Normal = Template.bind({});
+Normal.args = {
 };
