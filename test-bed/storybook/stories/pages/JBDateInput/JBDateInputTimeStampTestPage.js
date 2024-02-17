501b249bc1da92f28ec58bc8736a3ef64a804eeb
javadbat <javadbat@gmail.com>
2024-02-17 14:12:34 +0330

    fix import statement

--- test-bed/storybook/stories/pages/JBDateInput/JBDateInputTimeStampTestPage.js
+++ test-bed/storybook/stories/pages/JBDateInput/JBDateInputTimeStampTestPage.js
@@ -1,5 +1,5 @@
 import React, {useState, useMemo, useCallback} from 'react';
-import JBDateInput from 'jb-date-input-react';
+import {JBDateInput} from 'jb-date-input-react';
 function JBDateInputTimeStampTest(props) {
     const [setValue, setValueSetter] = useState(null);
     const valueInDate = useMemo(()=>{
