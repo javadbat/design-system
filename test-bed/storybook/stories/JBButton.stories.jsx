0395327e207395e98cc04611d4349aff89b363bd
javadbat <javadbat@gmail.com>
2024-05-17 12:01:25 +0330

    convert js to jsx for deno publish

---
+++ test-bed/storybook/stories/JBButton.stories.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import { JBButton } from "../../../react-component/jb-button/";
+
+export default {
+  title: "Example/JBButton",
+  component: JBButton,
+};
+
+const Template = (args) => <JBButton {...args}>{args.buttonText}</JBButton>;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  type: "primary",
+  buttonText: "ثبت محتوی",
+  loadingText: "",
+  disabled: false,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  type: "secendary",
+  buttonText: "ثبت اطلاعات",
+  loadingText: "",
+};
+export const Disabled = Template.bind({});
+Disabled.args = {
+  type: "secendary",
+  buttonText: "ثبت اطلاعات",
+  loadingText: "",
+  disabled: true,
+};
+export const Loading = Template.bind({});
+Loading.args = {
+  type: "primary",
+  buttonText: "ثبت محتوی",
+  isLoading: true,
+  loadingText: "لطفا صبر کنید",
+};
+export const LoadingSecondary = Template.bind({});
+LoadingSecondary.args = {
+  type: "secendary",
+  buttonText: "ثبت اطلاعات",
+  isLoading: true,
+  loadingText: "لطفا صبر کنید",
+};
---
+++ test-bed/storybook/stories/pages/jb-grid/JBGridTest.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './JBGridTest.css';
+import { Cell, Row, JBGrid, ExpandRow } from 'jb-grid';
+
+import JBGridBridge from './JBGridBridge';
+import JBGridTestViewModel from './JBGridTestViewModel';
+import { observer } from 'mobx-react';
+import ExpandRowSample from './ExpandRowSample';
+class JBGridTest extends Component {
+  constructor(props) {
+    super(props);
+    this.vm = new JBGridTestViewModel();
+  }
+
+  render() {
+    const { vm } = this;
+    return (
+      <div className="grid-wrapper">
+        <JBGrid config={vm.jbGridConfig} bridge={JBGridBridge} title="لیست کاربران" searchbarConfig={vm.filterConfig}>
+          {
+            vm.jbGridConfig.data.data.map(
+              (item) => {
+                return (
+                  <React.Fragment key={item.id}>
+                    <Row>
+                      <Cell>{item.name}</Cell>
+                      <Cell>{item.age}</Cell>
+                      <Cell><button onClick={()=>{item.jbGridDetail.isExpanded = !item.jbGridDetail.isExpanded;}}>detail</button></Cell>
+                    </Row>
+                    <ExpandRow show={item.jbGridDetail.isExpanded}>
+                      <ExpandRowSample></ExpandRowSample>
+                    </ExpandRow>
+                  </React.Fragment>
+
+                );
+              }
+            )
+          }
+        </JBGrid>
+      </div>
+    );
+  }
+}
+export default observer(JBGridTest);
