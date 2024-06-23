8b591ff907c3a7d32e976c6e7feaa62f80981645
javadbat <javadbat@gmail.com>
2024-06-23 16:39:45 +0330

    add storybook run

--- tasks/start/start.ts
+++ tasks/start/start.ts
@@ -1,5 +1,7 @@
 // const mockServiceProcess = spawn(`npm`,['run','serve','--prefix','mock/'],{shell:true,stdio:'inherit'});
 
+import { generalConfig } from "../../config/general-config.ts";
+
 //TODO: currently it's not working due to lack of support of storybook in deno but we need to complete and refactor it later (it,s better to move all stories to their component folder)
 //see this: https://github.com/storybookjs/storybook/issues/23279
 const storyBookCommand = new Deno.Command("deno", {
@@ -7,11 +9,16 @@ const storyBookCommand = new Deno.Command("deno", {
     "run",
     "--allow-read",
     "--allow-sys",
+    "--allow-net",
+    "--allow-run",
+    "--allow-write",
     "--allow-env",
     "npm:storybook",
+    "dev"
   ],
   stdin: "piped",
   stdout: "inherit",
+  cwd:'test-bed/storybook'
 });
 const mockApiCommand = new Deno.Command("deno", {
   args: [
@@ -22,4 +29,6 @@ const mockApiCommand = new Deno.Command("deno", {
   stdout: "inherit",
   cwd:"mock/"
 });
+Deno.env.set("STORYBOOK_PORT", generalConfig.port.storybookPort.toString());
 mockApiCommand.spawn();
+storyBookCommand.spawn();
