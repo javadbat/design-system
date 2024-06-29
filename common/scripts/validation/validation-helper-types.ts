9f692962139364aa44f880e66188654847414ea1
javadbat <javadbat@gmail.com>
2024-06-29 11:16:34 +0330

    update validation

--- common/scripts/validation/validation-helper-types.ts
+++ common/scripts/validation/validation-helper-types.ts
@@ -2,20 +2,21 @@ export type ValidationResultSummary = {
     isValid:boolean | null;
     message:string | null;
 }
-export type ValidationResultItem = {
+export type ValidationResultItem<ValidationValue> = {
     isValid:boolean | null;
     message:string | null;
-    validation:ValidationItem;
+    validation:ValidationItem<ValidationValue>;
 }
-export type ValidationResult = {
-    validationList:ValidationResultItem[];
+export type ValidationResult<ValidationValue> = {
+    validationList:ValidationResultItem<ValidationValue>[];
     isAllValid:boolean;
 }
 //TValue will be different for some inputs like date input
-export type ValidationItem<TValue = string> = {
-    validator: RegExp | ((value:TValue)=>boolean);
+export type ValidationItem<ValidationValue> = {
+    validator: RegExp | ((value:ValidationValue)=>boolean);
     message:string;
 }
 export type ShowValidationErrorCallback = (message:string)=>void;
 export type ClearValidationErrorCallback = ()=>void;
-export type GetInputtedTextCallback = ()=>string;
+export type GetInputtedTextCallback<ValidationValue> = ()=>ValidationValue;
+export type getValueStringCallback<ValidationValue> = (value:ValidationValue)=>string;
--- common/scripts/validation/validation-helper.ts
+++ common/scripts/validation/validation-helper.ts
@@ -1,37 +1,45 @@
-import {ClearValidationErrorCallback, GetInputtedTextCallback, ShowValidationErrorCallback, ValidationItem,ValidationResult,ValidationResultItem,ValidationResultSummary} from "./validation-helper-types";
+import {ClearValidationErrorCallback, GetInputtedTextCallback, ShowValidationErrorCallback, ValidationItem,ValidationResult,ValidationResultItem,ValidationResultSummary, getValueStringCallback} from "./validation-helper-types";
 
-export class ValidationHelper {
+export class ValidationHelper<ValidationValue>{
   //states
-  #validationList: ValidationItem[] = [];
-  validation?: ValidationResultSummary = {
+  #list: ValidationItem<ValidationValue>[] = [];
+  #resultSummary?: ValidationResultSummary = {
     isValid: null,
     message: null,
   };
+  get resultSummary(){
+    return this.#resultSummary;
+  }
+  result: ValidationResult<ValidationValue> | null = null;
   //callbacks
-  showValidationError: ShowValidationErrorCallback | null = null; 
-  clearValidationError: ClearValidationErrorCallback | null = null;
-  getInputText:GetInputtedTextCallback | null = null
-  constructor(showValidationError:ShowValidationErrorCallback, clearValidationError: ClearValidationErrorCallback, getInputText:GetInputtedTextCallback ) {
-    this.showValidationError = showValidationError;
-    this.clearValidationError= clearValidationError;
-    this.getInputText = getInputText;
+  #showValidationError: ShowValidationErrorCallback | null = null; 
+  #clearValidationError: ClearValidationErrorCallback | null = null;
+  #getInputText:GetInputtedTextCallback<ValidationValue> | null = null;
+  #getValueString:getValueStringCallback<ValidationValue> | null = null;
+  constructor(showValidationError:ShowValidationErrorCallback, clearValidationError: ClearValidationErrorCallback, getInputText:GetInputtedTextCallback<ValidationValue> ) {
+    this.#showValidationError = showValidationError;
+    this.#clearValidationError= clearValidationError;
+    this.#getInputText = getInputText;
   }
-  get validationList(): ValidationItem[] {
-    return this.#validationList;
+  setValueStringCallback(callback:getValueStringCallback<ValidationValue>){
+    this.#getValueString = callback;
   }
-  set validationList(value: ValidationItem[]) {
-    this.#validationList = value;
+  get list(): ValidationItem<ValidationValue>[] {
+    return this.#list;
+  }
+  set list(value: ValidationItem<ValidationValue>[]) {
+    this.#list = value;
     this.checkValidity(false);
   }
   /**
    * @description check if input validation list is fulfilled or not
    */
-  checkValidity(showError = true): ValidationResult {
+  checkValidity(showError = true): ValidationResult<ValidationValue> {
     // this method is for use out of component  for example if user click on submit button and developer want to check if all fields are valid
     //takeAction determine if we want to show user error in web component default Manner or developer will handle it by himself
-    const inputText = this.getInputText();
-    const validationResult = this.checkInputValidation(inputText);
-    this.validation = {
+    const inputText = this.#getInputText();
+    const validationResult = this.#checkValueValidation(inputText);
+    this.#resultSummary = {
       isValid: validationResult.isAllValid,
       message: null,
     };
@@ -39,22 +47,26 @@ export class ValidationHelper {
       const firstFault = validationResult.validationList.find(
         (x) => !x.isValid
       )!;
-      this.validation.message = firstFault.message;
+      this.resultSummary.message = firstFault.message;
       if (showError == true) {
-        this.showValidationError(firstFault.message!);
+        this.#showValidationError(firstFault.message!);
       }
     } else {
-      this.clearValidationError();
+      this.#clearValidationError();
     }
+    this.result = validationResult;
     return validationResult;
   }
-  checkInputValidation(value: string) {
-    const validationResult: ValidationResult = {
+  /**
+   *@description compare value with all validation
+   */
+  #checkValueValidation(value: ValidationValue) {
+    const validationResult: ValidationResult<ValidationValue> = {
       validationList: [],
       isAllValid: true,
     };
-    this.validationList.forEach((validation) => {
-      const res = this.checkValidation(value, validation);
+    this.list.forEach((validation) => {
+      const res = this.#checkValidation(value, validation);
       validationResult.validationList.push(res);
       if (!res.isValid) {
         validationResult.isAllValid = false;
@@ -62,15 +74,19 @@ export class ValidationHelper {
     });
     return validationResult;
   }
-  checkValidation(text: string,validation: ValidationItem): ValidationResultItem {
-    let testRes;
+  /**
+   * @description check single validation item
+   */
+  #checkValidation(value: ValidationValue,validation: ValidationItem<ValidationValue>): ValidationResultItem<ValidationValue> {
+    let testRes:boolean;
     if (validation.validator instanceof RegExp) {
+      const text = typeof value == "string"?value:this.#getValueString(value);
       testRes = validation.validator.test(text);
       validation.validator.lastIndex = 0;
     }
 
     if (typeof validation.validator == "function") {
-      testRes = validation.validator(text);
+      testRes = validation.validator(value);
     }
 
     if (!testRes) {
