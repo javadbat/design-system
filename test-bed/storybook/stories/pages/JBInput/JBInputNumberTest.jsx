6e84d9099b6f66e0a2998589fbd9f8748b31f2fb
javadbat <javadbat@gmail.com>
2024-07-21 00:22:36 +0330

    update number input

--- test-bed/storybook/stories/pages/JBInput/JBInputNumberTest.jsx
+++ test-bed/storybook/stories/pages/JBInput/JBInputNumberTest.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {JBInput} from '../../../../../react-component/jb-input';
+import {JBNumberInput} from 'jb-number-input-react';
 import "./JBInputNumberTest.css";
 
 const JBInputNumberTest = () => {
     return (
         <div className='input-wrapper'>
-            <JBInput type="number" label="تست" message="این متن تست می باشد"></JBInput>
+            <JBNumberInput  label="تست" message="این متن تست می باشد"></JBNumberInput>
         </div>
     );
 };
