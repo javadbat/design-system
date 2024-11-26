08e5099c11618c78e54a1fee0f362005894983fd
javadbat <javadbat@gmail.com>
2024-11-26 18:40:19 +0330

    add event bind

--- common/hooks/use-event.ts
+++ common/hooks/use-event.ts
@@ -14,3 +14,18 @@ export function useEvent(dom:HTMLElement, event:any, handler:any, passive = fals
     };
   });
 }
+export function useBindEvent<TRef extends React.MutableRefObject<any|null>,TEvent>(ref:TRef, event:string, handler:(e:TEvent)=>void, passive = false) {
+  useEffect(() => {
+    const dom = ref.current;
+    if (dom) {
+      // initiate the event handler
+      dom.addEventListener(event, handler, passive); 
+    }
+    // this will clean up the event every time the component is re-rendered
+    return function cleanup() {
+      if(dom){
+        dom.removeEventListener(event, handler, passive);
+      }
+    };
+  },[ref,event,handler,passive]);
+}
