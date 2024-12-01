7cf62da74e93991e867c93b0baa260670985671a
javadbat <javadbat@gmail.com>
2024-12-01 12:21:49 +0330

    update package

--- react-component/jb-searchbar/dist/common/hooks/use-event.d.ts
+++ react-component/jb-searchbar/dist/common/hooks/use-event.d.ts
@@ -1,1 +1,3 @@
+/// <reference types="react" />
 export declare function useEvent(dom: HTMLElement, event: any, handler: any, passive?: boolean): void;
+export declare function useBindEvent<TRef extends React.MutableRefObject<any | null>, TEvent>(ref: TRef, event: string, handler: (e: TEvent) => void, passive?: boolean): void;
