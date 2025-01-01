5c183b31f5d3447916537dd9ef8465789b4b1d5b
javadbat <javadbat@gmail.com>
2025-01-01 17:09:38 +0330

    update select

--- test-bed/storybook/stories/JBSelect.stories.jsx
+++ test-bed/storybook/stories/JBSelect.stories.jsx
@@ -10,7 +10,7 @@ export default {
   title: 'Example/JBSelect',
   component: JBSelect
 };
-const numberOptionList = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39']
+const numberOptionList = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39'];
 const Template = ({optionListProps,...args}) => <JBSelect { ...args}><JBOptionList {...optionListProps} /></JBSelect>;
 //replace and switch all test beween Template & WithOptionTemplate
 const WithOptionTemplate = ({optionListProps,...args}) => (
@@ -91,7 +91,7 @@ EmptyList.args = {
   label:'empty list',
   message:"this list is a empty list",
   optionListProps:{
-    optionList:[1],
+    optionList:[],
   }
 };
 
