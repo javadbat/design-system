c2328ce4c091f02a24eeb6e8eae89f03081303e5
javadbat <javadbat@users.noreply.github.com>
2025-05-09 15:33:31 +0000

    deploy: 8c42d87e52c3594a8e07dcf31de96897a18d64f4

---
+++ assets/introduction-paB5rNg1.js
@@ -0,0 +1,31 @@
+import{j as e,M as o}from"./index-BiK32LWi.js";import{useMDXComponents as i}from"./index-B8rYRX2K.js";import"./iframe-CoLaaSTP.js";import"./index-D4lIrffr.js";import"./index-BQQLSK9g.js";import"./index-DsJinFGm.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";function s(t){const n={a:"a",br:"br",h1:"h1",h2:"h2",li:"li",ol:"ol",p:"p",ul:"ul",...i(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Getting Started/Introduction"}),`
+`,e.jsx(n.h1,{id:"jb-design-system",children:"JB Design System"}),`
+`,e.jsx(n.p,{children:"Collection of necessary components to build a web Application with web-components & react components."}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"works on every Js framework and library like React, Vue, Angular, Svelte, etc."}),`
+`,e.jsx(n.li,{children:"dependency free, no need to install any other library or framework."}),`
+`,e.jsx(n.li,{children:"mostly focus on form elements and complicated elements with high user interactivity."}),`
+`,e.jsx(n.li,{children:'all components are written in web-component with additional React component wrapper around them(access them with "component-name/react").'}),`
+`,e.jsx(n.li,{children:"first class support for RTL and persian i18n utilities."}),`
+`]}),`
+`,e.jsx(n.p,{children:"we have 2 kind of components here:"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:["form base component:",e.jsx(n.br,{}),`
+`,"used in form to get values from user."]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:["non-form components:",e.jsx(n.br,{}),`
+`,"everything that can't be  used inside forms."]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.h2,{id:"architecture",children:"Architecture"}),`
+`,e.jsxs(n.p,{children:["unlike most Design systems, JB Design System is not a single big package that contain all the components and modules. instead we use separate package/repo for each component.",e.jsx(n.br,{}),`
+`,"this architecture lets user to select and install the package he need  without forcing him to download all the package he doesn't need and making it easier to update packages and manage dependencies.",e.jsx(n.br,{}),`
+`,"you can see the package list with their Repo address from ",e.jsx(n.a,{href:"?path=/docs/general-package-list--docs",children:"here"})]}),`
+`,e.jsx(n.h2,{id:"styling-guide",children:"Styling Guide"}),`
+`,e.jsx(n.p,{children:"Due to the nature of web-components and shadow DOM we have to use css native ways like custom properties to style our components."}),`
+`,e.jsxs("p",{children:[e.jsx(n.p,{children:"here is some useful links to get you started:"}),e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"?path=/docs/theme-colors--docs",children:"color system"})}),`
+`,e.jsx(n.li,{children:e.jsx(n.a,{href:"?path=/docs/theme-sizes--docs",children:"sizing system"})}),`
+`]})]})]})}function u(t={}){const{wrapper:n}={...i(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(s,{...t})}):s(t)}export{u as default};
