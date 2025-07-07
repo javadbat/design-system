9ba799cb6bebd7a3828742140fb501380af019e1
javadbat <javadbat@users.noreply.github.com>
2025-07-07 16:12:50 +0000

    deploy: 81678e08e9d6432c653f65873017e549f588e451

---
+++ assets/react-readme-SLMZglIH.js
@@ -0,0 +1,43 @@
+import{j as n,M as a,m as s}from"./index-CfkWLB4I.js";import{useMDXComponents as i}from"./index-B8rYRX2K.js";import"./iframe-kqisvmoX.js";import"./index-D4lIrffr.js";import"./index-Dc97iC8r.js";import"./index-DsJinFGm.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";const r=`# jb-switch React component
+
+switch react component.
+
+> this component is a react wrapper for [jb-switch](https://github.com/javadbat/jb-switch)
+
+sample:<https://codepen.io/javadbat/pen/jOyXKNJ>
+
+
+Demo :  Demo: [codeSandbox preview](https://3f63dj.csb.app/samples/jb-switch) for just see the demo and [codeSandbox editor](https://codesandbox.io/p/sandbox/jb-design-system-3f63dj?file=%2Fsrc%2Fsamples%2FJBSwitch.tsx) if you want to see and play with code
+
+## installation
+
+run \`npm install jb-switch\` to install package with npm
+
+## usage
+
+import component in your page \`import {JBSwitch} from 'jb-switch/react'\`  
+
+use below syntax in your render function
+
+\`\`\`jsx
+<JBSwitch value={booleanValue} isLoading={isLoading} trueTitle='true caption' falseTitle='false caption' onChange={(e)=>onChange(e)} ></JBSwitch>
+\`\`\`
+
+as you can see in above example \`value\` is on-way binding like normal react forms input trueTitle is a title that place on true side of component and get bold on \`value == true\` onChange is standard too  
+if you want to show loading in switch just make \`isLoading = true\`
+
+\`\`\`javascript
+ onChange(e){
+     setIsLoading(true)
+    return new Promise((resolve,reject)=>{
+        fetch(request).then((response)=>return response.json).then((data)=>{
+            setIsLoading(false)
+            resolve();
+        })
+    })
+}
+\`\`\`
+
+demo image:    
+![](./demo-gif.gif)`;function o(e){return n.jsxs(n.Fragment,{children:[n.jsx(a,{title:"Components/form elements/JBSwitch/React Readme"}),`
+`,n.jsx(s,{children:r})]})}function w(e={}){const{wrapper:t}={...i(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o()}export{w as default};
---
+++ assets/readme-B1S2uUay.js
@@ -0,0 +1,29 @@
+import{j as n,M as s,m as r}from"./index-CfkWLB4I.js";import{useMDXComponents as a}from"./index-B8rYRX2K.js";import"./iframe-kqisvmoX.js";import"./index-D4lIrffr.js";import"./index-Dc97iC8r.js";import"./index-DsJinFGm.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";const i=`# jb-core
+
+core modules of jb-design system mostly contain functions that help you manage your web-components & tools to connect them to ReactJS components.
+
+## js modules
+
+### listenAndSilentEvent
+
+this function listen to event in the capture phase and stop it's propagation and call your handler so you will be the only one who capture this event used for event forwarding (transformation) in web-components.
+
+\`\`\`js
+  listenAndSilentEvent(inputDom, 'keyup', yourOnKeyUpHandler);
+  listenAndSilentEvent(inputDom, 'keyup', yourOnKeyUpHandler,{passive:true});
+\`\`\`
+
+### create events
+
+these functions will create events base on existing event objects used mostly for event forwarding.
+
+\`\`\`ts
+  #onInputInput(e:InputEvent){
+    const event = createInputEvent('input', e, { cancelable: false });
+    this.dispatchEvent(event);
+  }
+    
+\`\`\`
+we also have \`createKeyboardEvent\`, \`createInputEvent\`, \`createFocusEvent\`.
+`;function o(e){return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"Components/JBCore/Readme"}),`
+`,n.jsx(r,{children:i})]})}function y(e={}){const{wrapper:t}={...a(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o()}export{y as default};
