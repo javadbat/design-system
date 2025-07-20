d5dffdfdd4b6862016ef07069624476638f69ae2
javadbat <javadbat@users.noreply.github.com>
2025-07-20 10:38:30 +0000

    deploy: d4ecc522d8d534b1513061c3c3522549d99e3d9f

--- assets/Overview-Ci1qZ9vm.js
+++
@@ -1,7 +0,0 @@
-import{j as t,M as e,T as i,S as a,C as m,a as p}from"./index-COXU5mFu.js";import{useMDXComponents as s}from"./index-B8rYRX2K.js";import{C as x,P as c}from"./JBButton.stories-CUYZAGKi.js";import"./iframe-tjpzAF6_.js";import"./index-D4lIrffr.js";import"./index-Dc97iC8r.js";import"./index-DsJinFGm.js";import"./index-CXQShRbs.js";import"./index-DrFu-skq.js";import"./JBButton-CCEmZSUB.js";import"./jb-loading-DQPCEDsW.js";import"./index-B65NTg4l.js";function r(n){const o={h2:"h2",p:"p",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:x}),`
-`,t.jsx(i,{children:"JB Button"}),`
-`,t.jsx(o.p,{children:"Button Component with loading and form integrate ability."}),`
-`,t.jsx(a,{of:c}),`
-`,t.jsx(m,{}),`
-`,t.jsx(o.h2,{id:"other-samples--variants",children:"Other samples & variants"}),`
-`,t.jsx(p,{})]})}function D(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{D as default};
