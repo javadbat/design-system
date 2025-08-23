55286831ecafd398f725d534e2e372a1eff750fd
javadbat <javadbat@users.noreply.github.com>
2025-08-23 15:27:35 +0000

    deploy: 20876e0ab270412288650d82e156f35a6347f187

---
+++ assets/react-readme-B0dZzPBN.js
@@ -0,0 +1,20 @@
+import{j as n,M as r,m as o}from"./index-CKMAxIRN.js";import{useMDXComponents as m}from"./index-CpNkpSEJ.js";import"./iframe-BUdcblGi.js";import"./index-0yr9KlQE.js";import"./index-tymuTqDW.js";import"./index-DotmK1FS.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";const s=`# jb-calendar React component
+
+calendar component for react 
+
+## installation
+
+run \`npm install jb-calendar\` to install package with npm
+
+## usage
+
+use below syntax in your render function.
+
+\`\`\`jsx
+import {JBCalendar} from 'jb-calendar/react';
+
+<JBCalendar></JBCalendar>
+\`\`\`
+
+`;function a(t){return n.jsxs(n.Fragment,{children:[n.jsx(r,{title:"Components/JBCalendar/React Readme"}),`
+`,n.jsx(o,{children:s})]})}function f(t={}){const{wrapper:e}={...m(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(a,{...t})}):a()}export{f as default};
