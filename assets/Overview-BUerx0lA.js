1b33c33335269c2bea6900cb578530a96ab5939b
javadbat <javadbat@users.noreply.github.com>
2025-08-25 20:10:27 +0000

    deploy: 58d65ebc4b9957284d9b2925aa2625ffa08ac0d9

--- assets/Overview-BUerx0lA.js
+++
@@ -1,5 +0,0 @@
-import{j as t,M as s,T as m,S as i,C as p}from"./index-0Pd01vxM.js";import{useMDXComponents as r}from"./index-CpNkpSEJ.js";import{D as a,N as c}from"./JBQRCode.stories-DOEEH1Ri.js";import"./iframe-Cg--1avl.js";import"./index-0yr9KlQE.js";import"./index-tymuTqDW.js";import"./index-DotmK1FS.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./index-Dsu68ezD.js";function e(o){const n={p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:a}),`
-`,t.jsx(m,{children:"JB QR Code"}),`
-`,t.jsx(n.p,{children:"QRcode display component"}),`
-`,t.jsx(i,{of:c,height:"30rem"}),`
-`,t.jsx(p,{})]})}function y(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{y as default};
