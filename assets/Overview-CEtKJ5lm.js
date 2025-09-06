bb5186f924689c3d07736d08bdd259a2a1bfc428
javadbat <javadbat@users.noreply.github.com>
2025-09-06 10:18:02 +0000

    deploy: d1cd25b2fd3992365d9707ea7180138c73ba358a

--- assets/Overview-CEtKJ5lm.js
+++
@@ -1,7 +0,0 @@
-import{j as t,M as e,T as a,S as i,C as m,a as p}from"./index-DazsERE5.js";import{useMDXComponents as s}from"./index-CpNkpSEJ.js";import{C as l,N as x}from"./JBCalendar.stories-Cth4NrX6.js";import"./iframe-D5VybveX.js";import"./index-0yr9KlQE.js";import"./index-tymuTqDW.js";import"./index-DotmK1FS.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./jb-calendar-8uGDlP10.js";import"./index-DERdVVTI.js";import"./index-Dsu68ezD.js";function r(n){const o={h2:"h2",p:"p",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:l}),`
-`,t.jsx(a,{children:"JB Calendar"}),`
-`,t.jsx(o.p,{children:"Jalali & Gregorian Calendar Component"}),`
-`,t.jsx(i,{of:x}),`
-`,t.jsx(m,{}),`
-`,t.jsx(o.h2,{id:"other-samples--variants",children:"Other samples & variants"}),`
-`,t.jsx(p,{})]})}function v(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{v as default};
