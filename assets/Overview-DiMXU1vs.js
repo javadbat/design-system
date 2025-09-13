a78dd1127ca658caefe20756e77101990d5560eb
javadbat <javadbat@users.noreply.github.com>
2025-09-13 16:21:41 +0000

    deploy: 3006c7ec43b66e3d2e5c3276787006309e40713e

--- assets/Overview-DiMXU1vs.js
+++
@@ -1,7 +0,0 @@
-import{j as t,M as e,T as a,S as i,C as m,a as p}from"./index-C1bX0aOk.js";import{useMDXComponents as s}from"./index-CpNkpSEJ.js";import{C as l,N as x}from"./JBCalendar.stories-DwKioi_3.js";import"./iframe-BTrbXZXE.js";import"./index-0yr9KlQE.js";import"./index-tymuTqDW.js";import"./index-DotmK1FS.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./jb-calendar-BH6WNwh_.js";import"./index-C3i7CD6V.js";import"./index-Dsu68ezD.js";function r(n){const o={h2:"h2",p:"p",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:l}),`
-`,t.jsx(a,{children:"JB Calendar"}),`
-`,t.jsx(o.p,{children:"Jalali & Gregorian Calendar Component"}),`
-`,t.jsx(i,{of:x}),`
-`,t.jsx(m,{}),`
-`,t.jsx(o.h2,{id:"other-samples--variants",children:"Other samples & variants"}),`
-`,t.jsx(p,{})]})}function v(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(r,{...n})}):r(n)}export{v as default};
---
+++ assets/react-readme-W6yk2rnE.js
@@ -0,0 +1,53 @@
+import{j as n,M as s,m as a}from"./index-Cgwh8mOk.js";import{useMDXComponents as i}from"./index-CpNkpSEJ.js";import"./iframe-0aqlSHb9.js";import"./index-0yr9KlQE.js";import"./index-tymuTqDW.js";import"./index-DotmK1FS.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";const r=`# JBButton React
+
+this component is React.js wrapper for [jb-button](https://www.npmjs.com/package/jb-button) web component
+JBButton is a simple button \`react component\` with some additional feature.
+
+- custom styling
+
+- loading state
+
+sample in codepen: <https://codepen.io/javadbat/pen/NWdeMwY>
+
+Demo :  Demo: [codeSandbox preview](https://3f63dj.csb.app/samples/jb-button) for just see the demo and [codeSandbox editor](https://codesandbox.io/p/sandbox/jb-design-system-3f63dj?file=%2Fsrc%2Fsamples%2FJBButton.tsx) if you want to see and play with code
+
+## installation
+
+\`\`\`sh
+    npm i jb-button
+\`\`\`
+in your jsx file
+\`\`\`js
+    import {JBButton} from "jb-button/react"
+\`\`\`
+\`\`\` jsx
+    <JBButton></JBButton>
+\`\`\`
+## instruction
+#### set loading
+
+you can show loading by setting props , isLoading = true you can add text to loading by <JBButton loading-text="...your loading text">your button text</JBButton>
+
+#### other props
+
+| props name	 | description                                      |
+| -------------  | -----------------------                          |
+| type           | change button HTML type                          |
+| disabled       | disable the button                               |
+| isLoading      | set loading state of button                      |
+| color          | 'primary', 'positive', 'danger', 'light', 'dark' |
+| variant        | 'solid', 'outline', 'ghost', 'text'              |
+| size           | 'xs' , 'sm' , 'md' , 'lg' , 'xl'                 |
+
+### events
+- onClick
+\`\`\`jsx
+  <JBButton onClick={(event) => console.log(event)}></JBButton>
+\`\`\`
+## change button style
+    react component depends on css variable for customization so there is no difference between react component and web-component in styling . to learn more about styling jb-button see [jb-button](https://github.com/javadbat/jb-button) styling section.
+
+
+
+`;function e(t){return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"Components/form elements/JBButton/React Readme"}),`
+`,n.jsx(a,{children:r})]})}function g(t={}){const{wrapper:o}={...i(),...t.components};return o?n.jsx(o,{...t,children:n.jsx(e,{...t})}):e()}export{g as default};
