0529411f5fc6a1a58b064828f932d6e9fb2f8ddd
javadbat <javadbat@users.noreply.github.com>
2025-09-14 06:45:49 +0000

    deploy: ac6e713a70a0662a891bd520937d6df2f2d31977

---
+++ assets/react-readme-P1t5bZFo.js
@@ -0,0 +1,26 @@
+import{j as t,M as a,m as i}from"./index-CQumYRii.js";import{useMDXComponents as s}from"./index-CpNkpSEJ.js";import"./iframe-CFd7j1wx.js";import"./index-0yr9KlQE.js";import"./index-tymuTqDW.js";import"./index-DotmK1FS.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";const r=`# jb-national-input-react
+
+react component wrapper for [jb-national-input](https://github.com/javadbat/jb-national-input)
+superset component on [jb-input-react](https://github.com/javadbat/jb-input-react) , just for national number input with extra filter and ready to use validator.
+
+کامپوننت ورود کد ملی برای ریاکت (ReactJS)
+
+
+Demo: [codeSandbox preview](https://3f63dj.csb.app/samples/jb-national-input) for just see the demo and [codeSandbox editor](https://codesandbox.io/p/sandbox/jb-design-system-3f63dj?file=%2Fsrc%2Fsamples%2FJBNationalInput.tsx) if you want to see and play with code
+## usage
+
+\`\`\`jsx
+import {JBNationalInput} from 'jb-national-input/react';
+
+<JBNationalInput></JBNationalInput>
+\`\`\`
+for more information and more detail please read [jb-input/react](https://github.com/javadbat/jb-input/tree/main/react) doc. all feature of jb-input is also available here.
+
+## Other Related Docs:
+
+- see [jb-national-input](https://github.com/javadbat/jb-national-input) if you want to use this component as a web-component.
+
+- see [All JB Design system Component List](https://javadbat.github.io/design-system/) for more components
+
+- use [Contribution Guide](https://github.com/javadbat/design-system/blob/main/docs/contribution-guide.md) if you want to contribute in this component.`;function o(n){return t.jsxs(t.Fragment,{children:[t.jsx(a,{title:"Components/form elements/Inputs/JBNationalInput/React Readme"}),`
+`,t.jsx(i,{children:r})]})}function h(n={}){const{wrapper:e}={...s(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o()}export{h as default};
