06eb092ef22bc89eed829055827db267179a75c9
javadbat <javadbat@users.noreply.github.com>
2025-09-18 20:45:11 +0000

    deploy: 477632d8ea1e7d39f46ed7efef1da6a2eeeeb6dc

--- assets/Overview-DF6s8SpS.js
+++
@@ -1,5 +0,0 @@
-import{j as t,M as i,T as m,S as p,C as s}from"./index-WZXUZgGT.js";import{useMDXComponents as e}from"./index-CpNkpSEJ.js";import{D as a,N as c}from"./JBDateInput.stories-DRAd4GGr.js";import"./iframe-RGE62y7Z.js";import"./index-0yr9KlQE.js";import"./index-tymuTqDW.js";import"./index-DotmK1FS.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./JBDateInput-YpDNBpM7.js";import"./jb-date-input-ColYZGvx.js";import"./jb-calendar-DKS8aW2i.js";import"./index-_cW7E40i.js";import"./index-C1o-zhIV.js";import"./index-DuXbOla-.js";import"./jb-validation-BFHHOeHY.js";import"./jb-popover-Cof9uiHy.js";function n(o){const r={p:"p",...e(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:a}),`
-`,t.jsx(m,{children:"JB Date Input"}),`
-`,t.jsx(r.p,{children:"jalali & gregorian date input component."}),`
-`,t.jsx(p,{of:c,height:"30rem"}),`
-`,t.jsx(s,{})]})}function _(o={}){const{wrapper:r}={...e(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{_ as default};
---
+++ assets/readme-DhqOfWqZ.js
@@ -0,0 +1,46 @@
+import{j as n,M as s,m as a}from"./index-CXXzJVXI.js";import{useMDXComponents as r}from"./index-CpNkpSEJ.js";import"./iframe-4nOiKzR6.js";import"./index-0yr9KlQE.js";import"./index-tymuTqDW.js";import"./index-DotmK1FS.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";const i=`# jb-core
+
+core modules of jb-design system mostly contain functions that help you manage your web-components & tools to connect them to ReactJS components.
+
+
+## React Modules
+
+to see react modules please see [\`jb-core/react\`](https://github.com/javadbat/jb-core/tree/main/react)
+
+## listenAndSilentEvent
+
+this function listen to event in the capture phase and stop it's propagation and call your handler so you will be the only one who capture this event used for event forwarding (transformation) in web-components.
+
+\`\`\`js
+  listenAndSilentEvent(inputDom, 'keyup', yourOnKeyUpHandler);
+  listenAndSilentEvent(inputDom, 'keyup', yourOnKeyUpHandler,{passive:true});
+\`\`\`
+
+## create events
+
+these functions will create events base on existing event objects used mostly for event forwarding.
+
+\`\`\`ts
+  #onInputInput(e:InputEvent){
+    const event = createInputEvent('input', e, { cancelable: false });
+    this.dispatchEvent(event);
+  }
+    
+\`\`\`
+we also have \`createKeyboardEvent\`, \`createInputEvent\`, \`createFocusEvent\`.
+
+## Inject Style
+
+when you have your styles in a string and you want to inject it in document or shadowroot you can use this function.
+
+\`\`\`ts
+import CSS from './style.css';
+import {injectCss} from 'jb-core';
+
+injectCss(CSS);
+// same as
+injectCss(CSS, document);
+// in web-components when you want inject your style inside shadow DOM
+injectCss(CSS, this.shadowRoot);
+\`\`\``;function o(e){return n.jsxs(n.Fragment,{children:[n.jsx(s,{title:"Components/JBCore/Readme"}),`
+`,n.jsx(a,{children:i})]})}function j(e={}){const{wrapper:t}={...r(),...e.components};return t?n.jsx(t,{...e,children:n.jsx(o,{...e})}):o()}export{j as default};
