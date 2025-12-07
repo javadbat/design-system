b8e2761644d39bcb0c0272f57308a1ebbcbc20c7
javadbat <javadbat@users.noreply.github.com>
2025-12-07 06:56:30 +0000

    deploy: 9e0572027b2f8e6af2b6446815b60531b1dd82fe

---
+++ assets/Overview-BLTtc8vD.js
@@ -0,0 +1,5 @@
+import{j as t,M as e,T as m,S as p,C as s}from"./index-DNissRHa.js";import{useMDXComponents as i}from"./index-CH6z84fh.js";import{D as a,N as c}from"./JBDateInput.stories-B20wRq2h.js";import"./iframe-MzgXJkao.js";import"./index-DP23ewiS.js";import"./index-BGsmrW1E.js";import"./index-FCbevRKN.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./JBDateInput-D7pLlWrM.js";import"./jb-date-input-D9cLifPI.js";import"./jb-calendar-B71Dk-uY.js";import"./index-C-kj9kL4.js";import"./index-DA-aheCX.js";import"./index-NH_qbYOC.js";import"./index-BlNJDujU.js";import"./jb-validation-BFHHOeHY.js";import"./jb-popover-Cb7kzQ85.js";function n(o){const r={p:"p",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(e,{of:a}),`
+`,t.jsx(m,{children:"JB Date Input"}),`
+`,t.jsx(r.p,{children:"jalali & gregorian date input component."}),`
+`,t.jsx(p,{of:c,height:"30rem"}),`
+`,t.jsx(s,{})]})}function w(o={}){const{wrapper:r}={...i(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(n,{...o})}):n(o)}export{w as default};
