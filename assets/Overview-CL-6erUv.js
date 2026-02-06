a922f22bb844b20d122af598f2f97ace9b82a32d
javadbat <javadbat@users.noreply.github.com>
2026-02-06 14:51:19 +0000

    deploy: 3dcbfaf559080c5134e9ec7f3e56775cd313ca18

---
+++ assets/Overview-CL-6erUv.js
@@ -0,0 +1,5 @@
+import{j as t,M as s,T as m,S as i,C as p}from"./index-CFSn5dqC.js";import{useMDXComponents as r}from"./index-DVgGKjXv.js";import{D as a,N as c}from"./JBQRCode.stories-BCp9Ws0Z.js";import"./iframe-Befpusgx.js";import"./index-DwQS_Y10.js";import"./index-CRFEHIza.js";import"./index-Bls5tne7.js";import"./index-DgH-xKnr.js";import"./index-DrFu-skq.js";import"./index-BJ5IkxrY.js";function e(o){const n={p:"p",...r(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:a}),`
+`,t.jsx(m,{children:"JB QR Code"}),`
+`,t.jsx(n.p,{children:"QRcode display component"}),`
+`,t.jsx(i,{of:c,height:"30rem"}),`
+`,t.jsx(p,{})]})}function y(o={}){const{wrapper:n}={...r(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{y as default};
---
+++ assets/Pagination.stories-1QKZD7N6.js
@@ -0,0 +1,12 @@
+import{S as i}from"./JBGrid-C5GOJ6Th.js";import"./index-DwQS_Y10.js";import"./index-_b1slotr.js";import"./index-CRFEHIza.js";import"./index-Bls5tne7.js";import"./jb-searchbar-sjgpOWDU.js";import"./index-C-sL0134.js";import"./jb-validation-DPnsTO1v.js";import"./index-BsliqPsk.js";import"./index-bfIR6bpK.js";import"./index-znC4IK4w.js";import"./jb-number-input-nl6rI1xW.js";import"./jb-date-input-vHDiMak5.js";import"./jb-calendar-DVcp7v1k.js";import"./jb-popover-DAtc3beD.js";import"./index-BJ5IkxrY.js";const B={title:"Components/JBGrid/Pagination",component:i},o={args:{pageIndex:1,onChange:p=>{console.log("change Event",p)}}},r={args:{max:10}};var e,a,t;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    pageIndex: 1,
+    onChange: e => {
+      console.log("change Event", e);
+    }
+  }
+}`,...(t=(a=o.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};var n,m,s;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    max: 10
+  }
+}`,...(s=(m=r.parameters)==null?void 0:m.docs)==null?void 0:s.source}}};const G=["Normal","Max"];export{r as Max,o as Normal,G as __namedExportsOrder,B as default};
