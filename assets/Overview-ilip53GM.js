24915c5963e095fe648fd49d34d2a14d90d8b1be
javadbat <javadbat@users.noreply.github.com>
2026-05-30 12:17:16 +0000

    deploy: ae7afe0ec88debfd97ccf41b1d38eb107acb7b62

--- assets/Overview-ilip53GM.js
+++
@@ -1,6 +0,0 @@
-import{n as t,g as m,T as p,i as s,c as a,S as c}from"./index-BIaH_qK_.js";import{useMDXComponents as i}from"./index-CSke7xmO.js";import{F as x,N as r}from"./JBFileInput.stories-CNRBL_zv.js";import"./iframe-BdvQ9nY1.js";import"./index-CEK5zJnf.js";import"./index-C2vOwVL5.js";import"./index-CupGaPSm.js";import"./index-B3ijS-tw.js";import"./index-DrFu-skq.js";import"./JBFileInput-BnOlScus.js";import"./jb-validation-AFHFNd1g.js";import"./index-BjRxPyWZ.js";import"./index-Bhe3Q7G8.js";import"./jb-loading-BuRiKsux.js";import"./jb-button-MYbfia63.js";import"./index-BtFglznY.js";import"./index-DJpgW01c.js";function e(o){const n={p:"p",...i(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(m,{of:x}),`
-`,t.jsx(p,{children:"JB File Input"}),`
-`,t.jsx(n.p,{children:"File Input Web Component & React Component."}),`
-`,t.jsx(s,{of:r}),`
-`,t.jsx(a,{}),`
-`,t.jsx(c,{of:r})]})}function T(o={}){const{wrapper:n}={...i(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(e,{...o})}):e(o)}export{T as default};
---
+++ assets/Pagination.stories-CJljWM2f.js
@@ -0,0 +1,12 @@
+import{C as i}from"./JBGrid-CVr2F5kZ.js";import"./index-CEK5zJnf.js";import"./index-Bhe3Q7G8.js";import"./index-C2vOwVL5.js";import"./index-CupGaPSm.js";import"./jb-searchbar-CI3RB4Sp.js";import"./index-BjRxPyWZ.js";import"./index-BP6bKaup.js";import"./jb-button-MYbfia63.js";import"./jb-loading-BuRiKsux.js";import"./index-BtFglznY.js";import"./jb-popover-CkK0yOS-.js";import"./jb-validation-AFHFNd1g.js";import"./index-DJpgW01c.js";const S={title:"Components/JBGrid/Pagination",component:i},o={args:{pageIndex:1,onChange:p=>{console.log("change Event",p)}}},r={args:{max:10}};var e,a,t;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    pageIndex: 1,
+    onChange: e => {
+      console.log("change Event", e);
+    }
+  }
+}`,...(t=(a=o.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};var n,m,s;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    max: 10
+  }
+}`,...(s=(m=r.parameters)==null?void 0:m.docs)==null?void 0:s.source}}};const _=["Normal","Max"];export{r as Max,o as Normal,_ as __namedExportsOrder,S as default};
