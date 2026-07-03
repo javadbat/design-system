552d6bcfcb1a316a26f90992c70a55b896ac53d1
javadbat <javadbat@gmail.com>
2026-07-04 00:12:24 +0330

    chore: update jb-grid

--- tasks/build/builder/src/plugins/lightningcss-string.ts
+++ tasks/build/builder/src/plugins/lightningcss-string.ts
@@ -6,38 +6,55 @@ const textEncoder = new TextEncoder();
 const textDecoder = new TextDecoder();
 const cssUrlRegExp = /\.css(?:[?#].*)?$/;
 const postfixRegExp = /[#?].*$/s;
+const inlineQueryRegExp = /[?&]inline(?:[=&?#]|$)/;
 
 function cleanUrl(url: string) {
   return url.replace(postfixRegExp, "");
 }
 
+function hasInlineQuery(url: string) {
+  return inlineQueryRegExp.test(url);
+}
+
+function transformCssToJs(code: string, id: string, options: LightningCSSStringOptions) {
+  const filename = cleanUrl(id);
+  const result = transform({
+    ...options,
+    filename,
+    code: textEncoder.encode(code),
+  });
+  const transformedCss = textDecoder.decode(result.code);
+
+  return {
+    code: `export default ${JSON.stringify(transformedCss)};`,
+    map: {
+      version: 3,
+      sources: [filename],
+      sourcesContent: [code],
+      names: [],
+      mappings: "",
+    },
+    moduleType: "js",
+  };
+}
+
 export function lightningCssString(options: LightningCSSStringOptions) {
   return {
     name: "lightningcss-string",
-    transform(code: string, id: string) {
-      if (!cssUrlRegExp.test(id) || id.includes("node_modules")) {
+    async load(id: string) {
+      if (!cssUrlRegExp.test(id) || !hasInlineQuery(id) || id.includes("node_modules")) {
         return null;
       }
 
-      const filename = cleanUrl(id);
-      const result = transform({
-        ...options,
-        filename,
-        code: textEncoder.encode(code),
-      });
-      const transformedCss = textDecoder.decode(result.code);
+      const code = await Deno.readTextFile(cleanUrl(id));
+      return transformCssToJs(code, id, options);
+    },
+    transform(code: string, id: string) {
+      if (!cssUrlRegExp.test(id) || hasInlineQuery(id) || id.includes("node_modules")) {
+        return null;
+      }
 
-      return {
-        code: `export default ${JSON.stringify(transformedCss)};`,
-        map: {
-          version: 3,
-          sources: [filename],
-          sourcesContent: [code],
-          names: [],
-          mappings: "",
-        },
-        moduleType: "js",
-      };
+      return transformCssToJs(code, id, options);
     },
   };
 }
